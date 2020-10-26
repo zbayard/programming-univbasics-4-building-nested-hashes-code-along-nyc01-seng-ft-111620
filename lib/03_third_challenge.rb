84c7fefbeacb1ea1509814d0b31bd62d3c3110c0
Zach Bayard <zbayard@gmail.com>
2020-10-26 18:20:28 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -11,7 +11,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet" , age: "50"
         #your key/value pairs here
@@ -26,5 +26,5 @@ def third_challenge
    }
   }
 
-  
+
 end

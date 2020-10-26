441ef6e29b140a5daaa8b56e22e4e41f501122fc
Zach Bayard <zbayard@gmail.com>
2020-10-26 18:21:08 +0000

    Automatically backed up by Learn

--- lib/03_third_challenge.rb
+++ lib/03_third_challenge.rb
@@ -1,7 +1,7 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {name: "Lord Montague" , age: "54"
+      :patriarch => {name: "Lord Montague" , age: "53"
         #your key/value pairs here
         },
       :matriarch => {name: "Lady Montague" , age: "54"
